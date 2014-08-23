f33a5c2d7b8b98e11b0db7fee097976a3180f1dd
dOkI <d0k1@focus-it.ru>
2014-08-23 17:31:37 +0600

    improovements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test");
-		final MainFrame frame = new MainFrame(20, 20);
+		final MainFrame frame = new MainFrame(50, 50);
 		final Engine eng = new Engine(frame.getGrid());
 
 		new Blinker(frame.getGrid());
